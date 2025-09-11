600422c718c07f2fc62d93ffa93e344f9aaf874c
Maqbool Ahmed <Maqbool.Ahmed@causeway.com>
2025-09-11 22:31:43 +0530

    Errors handled

--- project1/pdf-extract.py
+++ project1/pdf-extract.py
@@ -1,14 +1,27 @@
 from pypdf import PdfReader
+import os
 
 output_folder = "../content/"
 file_name = "Chemistry Questions.pdf"
-reader = PdfReader(output_folder + file_name)
 
-file_path = output_folder + "project1_output.txt"
+try:
+    file_path = output_folder + file_name
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+    
+    reader = PdfReader(file_path)
 
-with open(file_path, 'w') as file:
-    for i, page in enumerate(reader.pages, start=1):
-        text = page.extract_text()
-        file.write(text)
+    output_file_path = output_folder + "project1_output.txt"
 
-print(f"The {file_name}'s content extract has been stored at '{file_path}'")
+    with open(output_file_path, 'w') as file:
+        for i, page in enumerate(reader.pages, start=1):
+            text = page.extract_text()
+            file.write(text)
+
+    print(f"The {file_name}'s content extract has been stored at '{output_file_path}'")
+
+except FileNotFoundError as e:
+    print(e)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
